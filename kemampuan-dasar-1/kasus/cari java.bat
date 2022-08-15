20be77a27ff4471d6591b7f7fd35eb3a5132355f
wisnubuanas <king.wisnu50@gmail.com>
2022-08-15 16:28:28 +0700

    coding pgt

---
+++ kemampuan-dasar-1/kasus/cari java.bat
@@ -0,0 +1,11 @@
+@echo off
+
+cd D:\kemampuan-dasar\kemampuan-dasar-1\kasus
+
+echo Ada file Java pada direktori: D:\kemampuan-dasar\kemampuan-dasar-1\kasus
+
+dir D:\kemampuan-dasar\kemampuan-dasar-1\kasus
+
+dir /s *.java
+
+PAUSE
